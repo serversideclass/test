32a2bc328a312d9c3f2c93c60261b3a58e62026a
Kevin Waters <ec2-user@ip-172-31-24-92.us-east-2.compute.internal>
2018-03-25 10:19:34 +0000

    Routes added for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'static_pages/about', to: 'static_pages#about' 
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
