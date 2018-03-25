eed125ae466c9dbb6393091e68883c6c88410f42
Diana Nevedomska <diana_nevedomska@yahoo.com>
2018-03-25 11:30:49 +0000

    products

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
   root 'static_pages#home'
 
   get 'static_pages/about', to: 'static_pages#about' 
 
   get 'static_pages/contact', to: 'static_pages#contact'
+  
+ # get '/products', to: 'products#products'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20180325111308_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.string :category
+      t.integer :caloriesPerServing
+      t.boolean :glutenFree
+      t.boolean :containsNuts
+      t.text :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
